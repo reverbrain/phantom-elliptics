f29222492e3669addcd8eb5763bc15c55b10cfe2
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-04-10 20:47:26 +0400

    Removed unnecessary dependencies

--- cmake/Modules/FindElliptics.cmake
+++ cmake/Modules/FindElliptics.cmake
@@ -1,9 +1,8 @@
 find_path(ELLIPTICS_INCLUDE_DIR elliptics/cppdef.h PATHS ${ELLIPTICS_PREFIX}/include /usr/include)
 
-find_library(ELLIPTICS_LIBRARY NAMES elliptics PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 find_library(ELLIPTICS_cpp_LIBRARY NAMES elliptics_cpp PATHS ${ELLIPTICS_PREFIX}/lib ${ELLIPTICS_PREFIX}/lib64 /usr/lib /usr/lib64)
 
-set(ELLIPTICS_LIBRARIES ${ELLIPTICS_LIBRARY} ${ELLIPTICS_cpp_LIBRARY})
+set(ELLIPTICS_LIBRARIES ${ELLIPTICS_cpp_LIBRARY})
 set(ELLIPTICS_INCLUDE_DIRS ${ELLIPTICS_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
